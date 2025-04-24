4803fd6af69a4dfce1c9203560cc55f248153b75
Marlon <marlonmontenegropaz@gmail.com>
2025-04-24 14:21:21 -0600

    Actualizo .gitignore y agrego el script de base de datos

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,24 @@ package src;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        logger.info("Esto es un mensaje de INFO");
-        logger.warn("Esto es un mensaje de WARNING");
-        logger.error("Esto es un mensaje de ERROR simulado");
+        String url = "jdbc:mysql://localhost:3306/mediatecadb";
+        String user = "root";
+        String password = "";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión exitosa a MySQL!");
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
