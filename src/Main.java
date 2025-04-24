de1d06398b77ce33a28d3adf43b431ddab4032da
Marlon <marlonmontenegropaz@gmail.com>
2025-04-24 14:26:05 -0600

    Refactor main

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,10 @@
 package src;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class);
+
 
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/mediatecadb";
-        String user = "root";
-        String password = "";
 
-        try {
-            Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Conexión exitosa a MySQL!");
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
